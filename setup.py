7d7861e445b50040c198e3291a5cc9d889f4d5d8
Kobie-Kirven <51420086+Kobie-Kirven@users.noreply.github.com>
2022-03-07 11:34:52 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+# Author: Kobie Kirven
+# Penn State University
+# Date: 3-7-2022
+
+import setuptools
+import sys
+from pathlib import Path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+CURRENT_DIR = Path(__file__).parent
+sys.path.insert(0, str(CURRENT_DIR))
+
+
+setuptools.setup(
+    name="minetwister",
+    version="0.01",
+    packages=["bin", "bin/minetwister"],
+    include_package_data=True,
+    entry_points={"console_scripts": ["minetwister=bin.__main__:minetwister",],},
+    author="Kobie Kirven",
+    description="mineTwister: a tool for mining genomes for twister ribozymes",
+    install_requires=["setuptools", "biopython", "pandas"],
+    python_requires=">=3.5",
+)
